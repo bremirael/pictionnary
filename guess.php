3c0208961dae5175aba0002010fba1f9efcafd34
BERNATENE Gael <bg704531@iutnice.unice.fr>
2017-12-19 14:13:55 +0100

    version tag v1 19/12/2017

--- guess.php
+++ guess.php
@@ -28,9 +28,7 @@ if(!isset($_SESSION['id'])) {
     <script>
         // la taille et la couleur du pinceau
         var size, color;
-        // la dernière position du stylo
         var x0, y0;
-        // le tableau de commandes de dessin à envoyer au serveur lors de la validation du dessin
         var drawingCommands = <?php echo $commands;?>;
         console.log(drawingCommands);
         window.onload = function() {
